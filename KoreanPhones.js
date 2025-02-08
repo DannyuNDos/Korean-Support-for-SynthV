70053c4668d25ff0f6cebfc6d294578baf76c50d
DannyuNDos <ndospark320@naver.com>
2025-02-09 06:07:27 +0900

    Add files via upload

--- KoreanPhones.js
+++ KoreanPhones.js
@@ -156,24 +156,24 @@ function setToKorean(note) {
                         break;
                     case 9:
                         if (syllable_phonemes[0] == "#j") {
-                            syllable_phonemes = ["#s\\"].concat(syllable_phonemes.slice(1));
+                            syllable_phonemes = ["#s\\", "$x"].concat(syllable_phonemes.slice(1));
                         }
                         else if (["i", "y"].indexOf(syllable_phonemes[0]) >= 0) {
-                            syllable_phonemes = ["#s\\"].concat(syllable_phonemes);
+                            syllable_phonemes = ["#s\\", "$x"].concat(syllable_phonemes);
                         }
                         else {
-                            syllable_phonemes = ["#s"].concat(syllable_phonemes);
+                            syllable_phonemes = ["#s", "$x"].concat(syllable_phonemes);
                         }
                         break;
                     case 10:
                         if (syllable_phonemes[0] == "#j") {
-                            syllable_phonemes = ["%s\\"].concat(syllable_phonemes.slice(1));
+                            syllable_phonemes = ["s\\"].concat(syllable_phonemes.slice(1));
                         }
                         else if (["i", "y"].indexOf(syllable_phonemes[0]) >= 0) {
-                            syllable_phonemes = ["%s\\"].concat(syllable_phonemes);
+                            syllable_phonemes = ["s\\"].concat(syllable_phonemes);
                         }
                         else {
-                            syllable_phonemes = ["%s"].concat(syllable_phonemes);
+                            syllable_phonemes = ["s"].concat(syllable_phonemes);
                         }
                         break;
                     case 11:
@@ -275,24 +275,24 @@ function setToKorean(note) {
                         break;
                     case 9:
                         if (syllable_phonemes[0] == "#y") {
-                            syllable_phonemes = ["#sh"].concat(syllable_phonemes.slice(1));
+                            syllable_phonemes = ["#sh", "$hh"].concat(syllable_phonemes.slice(1));
                         }
                         else if (["iy"].indexOf(syllable_phonemes[0]) >= 0) {
-                            syllable_phonemes = ["#sh"].concat(syllable_phonemes);
+                            syllable_phonemes = ["#sh", "$hh"].concat(syllable_phonemes);
                         }
                         else {
-                            syllable_phonemes = ["#s"].concat(syllable_phonemes);
+                            syllable_phonemes = ["#s", "$hh"].concat(syllable_phonemes);
                         }
                         break;
                     case 10:
                         if (syllable_phonemes[0] == "#y") {
-                            syllable_phonemes = ["%sh"].concat(syllable_phonemes.slice(1));
+                            syllable_phonemes = ["sh"].concat(syllable_phonemes.slice(1));
                         }
                         else if (["iy"].indexOf(syllable_phonemes[0]) >= 0) {
-                            syllable_phonemes = ["%sh"].concat(syllable_phonemes);
+                            syllable_phonemes = ["sh"].concat(syllable_phonemes);
                         }
                         else {
-                            syllable_phonemes = ["%s"].concat(syllable_phonemes);
+                            syllable_phonemes = ["s"].concat(syllable_phonemes);
                         }
                         break;
                     case 11:
@@ -360,294 +360,13 @@ function setToKorean(note) {
                         break;
                 }
             }
-            else if ("japanese" == note.getLanguageOverride()) {
-                switch (onset) {
-                    case 0:
-                        if (syllable_phonemes[0] == "y") {
-                            syllable_phonemes = ["gy"].concat(syllable_phonemes.slice(1));
-                        }
-                        else {
-                            syllable_phonemes = ["g"].concat(syllable_phonemes);
-                        }
-                        break;
-                    case 1:
-                        if (syllable_phonemes[0] == "y") {
-                            syllable_phonemes = ["#cl", "#gy"].concat(syllable_phonemes.slice(1));
-                        }
-                        else {
-                            syllable_phonemes = ["#cl", "#g"].concat(syllable_phonemes);
-                        }
-                        break;
-                    case 2:
-                        if (syllable_phonemes[0] == "y") {
-                            syllable_phonemes = ["#ny"].concat(syllable_phonemes.slice(1));
-                        }
-                        else {
-                            syllable_phonemes = ["#n"].concat(syllable_phonemes);
-                        }
-                        break;
-                    case 3:
-                        if (syllable_phonemes[0] == "y") {
-                            syllable_phonemes = ["dy"].concat(syllable_phonemes.slice(1));
-                        }
-                        else {
-                            syllable_phonemes = ["d"].concat(syllable_phonemes);
-                        }
-                        break;
-                    case 4:
-                        if (syllable_phonemes[0] == "y") {
-                            syllable_phonemes = ["#cl", "#dy"].concat(syllable_phonemes.slice(1));
-                        }
-                        else {
-                            syllable_phonemes = ["#cl", "#d"].concat(syllable_phonemes);
-                        }
-                        break;
-                    case 5:
-                        if (syllable_phonemes[0] == "y") {
-                            syllable_phonemes = ["#ry"].concat(syllable_phonemes.slice(1));
-                        }
-                        else {
-                            syllable_phonemes = ["#r"].concat(syllable_phonemes);
-                        }
-                        break;
-                    case 6:
-                        if (syllable_phonemes[0] == "y") {
-                            syllable_phonemes = ["#my"].concat(syllable_phonemes.slice(1));
-                        }
-                        else {
-                            syllable_phonemes = ["#m"].concat(syllable_phonemes);
-                        }
-                        break;
-                    case 7:
-                        if (syllable_phonemes[0] == "y") {
-                            syllable_phonemes = ["by"].concat(syllable_phonemes.slice(1));
-                        }
-                        else {
-                            syllable_phonemes = ["b"].concat(syllable_phonemes);
-                        }
-                        break;
-                    case 8:
-                        if (syllable_phonemes[0] == "y") {
-                            syllable_phonemes = ["#cl", "#by"].concat(syllable_phonemes.slice(1));
-                        }
-                        else {
-                            syllable_phonemes = ["#cl", "#b"].concat(syllable_phonemes);
-                        }
-                        break;
-                    case 9:
-                        if (syllable_phonemes[0] == "y") {
-                            syllable_phonemes = ["#sh"].concat(syllable_phonemes.slice(1));
-                        }
-                        else if (["i"].indexOf(syllable_phonemes[0]) >= 0) {
-                            syllable_phonemes = ["#sh"].concat(syllable_phonemes);
-                        }
-                        else {
-                            syllable_phonemes = ["#s"].concat(syllable_phonemes);
-                        }
-                        break;
-                    case 10:
-                        if (syllable_phonemes[0] == "y") {
-                            syllable_phonemes = ["%sh"].concat(syllable_phonemes.slice(1));
-                        }
-                        else if (["i"].indexOf(syllable_phonemes[0]) >= 0) {
-                            syllable_phonemes = ["%sh"].concat(syllable_phonemes);
-                        }
-                        else {
-                            syllable_phonemes = ["%s"].concat(syllable_phonemes);
-                        }
-                        break;
-                    case 11:
-                        break;
-                    case 12:
-                        if (syllable_phonemes[0] == "y") {
-                            syllable_phonemes = ["j"].concat(syllable_phonemes.slice(1));
-                        }
-                        else {
-                            syllable_phonemes = ["j"].concat(syllable_phonemes);
-                        }
-                        break;
-                    case 13:
-                        if (syllable_phonemes[0] == "y") {
-                            syllable_phonemes = ["#cl", "#j"].concat(syllable_phonemes.slice(1));
-                        }
-                        else {
-                            syllable_phonemes = ["#cl", "#j"].concat(syllable_phonemes);
-                        }
-                        break;
-                    case 14:
-                        if (syllable_phonemes[0] == "y") {
-                            syllable_phonemes = ["ch"].concat(syllable_phonemes.slice(1));
-                        }
-                        else {
-                            syllable_phonemes = ["ch"].concat(syllable_phonemes);
-                        }
-                        break;
-                    case 15:
-                        if (syllable_phonemes[0] == "y") {
-                            syllable_phonemes = ["ky"].concat(syllable_phonemes.slice(1));
-                        }
-                        else {
-                            syllable_phonemes = ["k"].concat(syllable_phonemes);
-                        }
-                        break;
-                    case 16:
-                        if (syllable_phonemes[0] == "y") {
-                            syllable_phonemes = ["ty"].concat(syllable_phonemes.slice(1));
-                        }
-                        else {
-                            syllable_phonemes = ["t"].concat(syllable_phonemes);
-                        }
-                        break;
-                    case 17:
-                        if (syllable_phonemes[0] == "y") {
-                            syllable_phonemes = ["py"].concat(syllable_phonemes.slice(1));
-                        }
-                        else {
-                            syllable_phonemes = ["p"].concat(syllable_phonemes);
-                        }
-                        break;
-                    default:
-                        if (syllable_phonemes[0] == "y") {
-                            syllable_phonemes = ["hy"].concat(syllable_phonemes.slice(1));
-                        }
-                        else {
-                            syllable_phonemes = ["h"].concat(syllable_phonemes);
-                        }
-                        break;
-                }
-                switch (coda) {
-                    case 0:
-                        break;
-                    case 4: case 5: case 6:
-                        syllable_phonemes.push("n");
-                        break;
-                    case 8: case 11: case 12: case 13: case 15:
-                        syllable_phonemes.push("r");
-                        break;
-                    case 10: case 16:
-                        syllable_phonemes.push("m");
-                        break;
-                    case 21:
-                        syllable_phonemes.push("N");
-                        break;
-                    case 7: case 19: case 20: case 22: case 23: case 25: case 27:
-                        syllable_phonemes.push("d");
-                        break;
-                    case 14: case 17: case 18: case 26:
-                        syllable_phonemes.push("b");
-                        break;
-                    default:
-                        syllable_phonemes.push("cl");
-                        break;
-                }
-            }
-            else if ("cantonese" == note.getLanguageOverride()) {
-                switch (onset) {
-                    case 0:
-                        if (syllable_phonemes[0] == "w") {
-                            syllable_phonemes = ["kw"].concat(syllable_phonemes.slice(1));
-                        }
-                        else {
-                            syllable_phonemes = ["k"].concat(syllable_phonemes);
-                        }
-                        break;
-                    case 1:
-                        if (syllable_phonemes[0] == "w") {
-                            syllable_phonemes = ["#cl", "#kw"].concat(syllable_phonemes.slice(1));
-                        }
-                        else {
-                            syllable_phonemes = ["#cl", "#k"].concat(syllable_phonemes);
-                        }
-                        break;
-                    case 2:
-                        syllable_phonemes = ["#n"].concat(syllable_phonemes);
-                        break;
-                    case 3:
-                        syllable_phonemes = ["t"].concat(syllable_phonemes);
-                        break;
-                    case 4:
-                        syllable_phonemes = ["#cl", "#t"].concat(syllable_phonemes);
-                        break;
-                    case 5:
-                        syllable_phonemes = ["$t"].concat(syllable_phonemes);
-                        break;
-                    case 6:
-                        syllable_phonemes = ["#m"].concat(syllable_phonemes);
-                        break;
-                    case 7:
-                        syllable_phonemes = ["p"].concat(syllable_phonemes);
-                        break;
-                    case 8:
-                        syllable_phonemes = ["#cl", "#p"].concat(syllable_phonemes);
-                        break;
-                    case 9:
-                        syllable_phonemes = ["#s"].concat(syllable_phonemes);
-                        break;
-                    case 10:
-                        syllable_phonemes = ["%s"].concat(syllable_phonemes);
-                        break;
-                    case 11:
-                        break;
-                    case 12:
-                        syllable_phonemes = ["ts"].concat(syllable_phonemes);
-                        break;
-                    case 13:
-                        syllable_phonemes = ["#cl", "#ts"].concat(syllable_phonemes);
-                        break;
-                    case 14:
-                        syllable_phonemes = ["tsh"].concat(syllable_phonemes);
-                        break;
-                    case 15:
-                        if (syllable_phonemes[0] == "w") {
-                            syllable_phonemes = ["kwh"].concat(syllable_phonemes.slice(1));
-                        }
-                        else {
-                            syllable_phonemes = ["kh"].concat(syllable_phonemes);
-                        }
-                        break;
-                    case 16:
-                        syllable_phonemes = ["th"].concat(syllable_phonemes);
-                        break;
-                    case 17:
-                        syllable_phonemes = ["ph"].concat(syllable_phonemes);
-                        break;
-                    default:
-                        syllable_phonemes = ["h"].concat(syllable_phonemes);
-                        break;
-                }
-                switch (coda) {
-                    case 0:
-                        break;
-                    case 4: case 5: case 6:
-                        syllable_phonemes.push(":n");
-                        break;
-                    case 8: case 11: case 12: case 13: case 15:
-                        syllable_phonemes.push("l");
-                        break;
-                    case 10: case 16:
-                        syllable_phonemes.push(":m");
-                        break;
-                    case 21:
-                        syllable_phonemes.push(":N");
-                        break;
-                    case 7: case 19: case 20: case 22: case 23: case 25: case 27:
-                        syllable_phonemes.push(":t_}");
-                        break;
-                    case 14: case 17: case 18: case 26:
-                        syllable_phonemes.push(":p_}");
-                        break;
-                    default:
-                        syllable_phonemes.push(":k_}");
-                        break;
-                }
-            }
         }
         else if (lyrics[i] != ' ') {
             return;
         }
         phonemes = phonemes.concat(syllable_phonemes);
     }
-    note.setPhonemes(phonemes.join(" ").replaceAll(/#/, "").replaceAll(/\$/, "").replaceAll(/%/, ""));
+    note.setPhonemes(phonemes.join(" ").replaceAll(/#/, "").replaceAll(/\$/, ""));
     var note_durs = [];
     var note_strs = [];
     for (var i = 0; i < phonemes.length; ++i) {
@@ -659,10 +378,6 @@ function setToKorean(note) {
             case '$':
                 note_durs.push(0.3);
                 note_strs.push(1);
-                break;
-            case '%':
-                note_durs.push(1.8);
-                note_strs.push(1.8);
                 break;
             default:
                 note_durs.push(1);
